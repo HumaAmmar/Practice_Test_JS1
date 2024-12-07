454ad7e31782258f55857446ec321f2f3f1165cf
Huma Zia <rankwithhani@gmail.com>
2024-12-07 13:54:38 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// Javascript Chapter 1 Test
+
+// Answer of Question 1
+alert("Hello! Welcome to my Website");
+
+// Answer of Question 2
+alert("Error! Please enter a valid password");
+
+// Answer of Question 3
+alert("Welcome to JS land... \n Happy Coding!");
+
+// Answer of Question 4
+alert("Welcome to JS land...");
+alert("Happy Coding! \n Prevent this page from creating additional dialogues!");
+
+// Answer of Question 5
+console.log("Hello...I can run JS through my web browser's console")
+
+
