e18d74a02eba9d752a03680a17c9f42f2071e578
Huma Zia <rankwithhani@gmail.com>
2024-12-07 15:01:34 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+// Javascript Chapter 2 Test
+
+// Answer of Question 1 
+var username
+
+// Answer of Question 2
+var myName = "Huma Zia";
+
+// Answer of Question 3
+ var message 
+ var message = "Hello World";
+ alert(message);
+
+ // Answer of Question 4
+ alert("Student's Bio")
+ var studentName = "Huma Zia";
+ alert(studentName);
+ var studentAge = "27 yeras old";
+ alert(studentAge);
+ var skill = "Certified Web Developer";
+ alert(skill);
+
+ // Answer of Question 5
+ var pizaa = "Pizza \n Pizz \n Piz \n Pi \n P"
+ alert(pizaa);
+
+ // Answer of Question 6
+ var text = "my email address is..."
+ var email = "huma.zia@gmail.com";
+ var result = text.concat("", email);
+ alert(result);
+
+ // Answer of Question 7
+ var book = "A smarter way to learn JavaScript"
+ alert(book);
+
+ // Answer of Question 8
+ document.write("Yah! I can write HTML content through JavaScript");
+
+ // Answer of Question 9
+ document.write("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+
+ // Answer of Question 10
+ alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
