7ace53a1564b2a119fcb02a7f872557b13c7da32
Huma Zia <rankwithhani@gmail.com>
2024-12-07 14:52:35 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
-// Javascript Chapter 1 Test
+// Javascript Chapter 2 Test
 
-// Answer of Question 1
-alert("Hello! Welcome to my Website");
+// Answer of Question 1 
+var username
 
 // Answer of Question 2
-alert("Error! Please enter a valid password");
+var myName = "Huma Zia";
 
 // Answer of Question 3
-alert("Welcome to JS land... \n Happy Coding!");
+ var message 
+ var message = "Hello World";
+ alert(message);
 
-// Answer of Question 4
-alert("Welcome to JS land...");
-alert("Happy Coding! \n Prevent this page from creating additional dialogues!");
+ // Answer of Question 4
+ alert("Student's Bio")
+ var studentName = "Huma Zia";
+ alert(studentName);
+ var studentAge = "27 yeras old";
+ alert(studentAge);
+ var skill = "Certified Web Developer";
+ alert(skill);
 
-// Answer of Question 5
-console.log("Hello...I can run JS through my web browser's console")
+ // Answer of Question 5
+ var pizaa = "Pizza \n Pizz \n Piz \n Pi \n P"
+ alert(pizaa);
 
+ // Answer of Question 6
+ var text = "my email address is..."
+ var email = "huma.zia@gmail.com";
+ var result = text.concat("", email);
+ alert(result);
 
+ // Answer of Question 7
+ var book = "A smarter way to learn JavaScript"
+ alert(book);
+
+ // Answer of Question 8
+ document.write("Yah! I can write HTML content through JavaScript");
+
+ // Answer of Question 9
+ document.write("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+
+ // Answer of Question 10
+ alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
